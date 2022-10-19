210b1b37636419b21c8dc3180564f438a029eaf9
Evgeny <bril424@mail.ru>
2022-10-19 12:43:02 +0200

    first react

---
+++ react/lesson1/my_app/src/components/App.js
@@ -0,0 +1,13 @@
+import Example from "./Example";
+
+function App() {
+  return (
+    <div>
+      <Example Name={'Oleg'} lastName={'Petrov'}/>
+      <Example Name={'Olga'} lastName={'Burg'}/>
+      <Example Name={'Goga'} lastName={'Muller'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/lesson1/my_app/src/components/Example.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Example({Name, lastName}) {
+  return (
+    <div>
+       <p> Name:  {Name}</p>
+       <p> Lastname: {lastName} Petrov</p>
+    </div>
+  )
+}
