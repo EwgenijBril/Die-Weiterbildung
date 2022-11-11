bbf1836abf3ca528bffa89b8b8e0e791063b5034
Evgeny <bril424@mail.ru>
2022-11-11 11:01:45 +0100

    about as section added

--- react/lesson4_website/my_app/src/components/App/index.jsx
+++ react/lesson4_website/my_app/src/components/App/index.jsx
@@ -1,12 +1,15 @@
 import '../../style.css'
+import AboutUs from '../AboutUs';
 import Nav from '../Nav';
 import Services from '../Services';
+
 
 function App() {
   return (
     <div >
       <Nav/>
       <Services/>
+      <AboutUs/>
     </div>
   );
 }
