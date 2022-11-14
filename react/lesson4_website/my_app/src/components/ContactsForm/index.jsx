87fbd587eabb8a55b19d6ac5fb51eb7ebcf95f76
Evgeny <bril424@mail.ru>
2022-11-14 10:23:36 +0100

    Contacts add

--- react/lesson4_website/my_app/src/components/ContactsForm/index.jsx
+++ react/lesson4_website/my_app/src/components/ContactsForm/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import s from './index.module.css'
 
 export default function ContactsForm() {
   return (
-    <div>
-        
-    </div>
+    <form className={s.contacts_form}>
+      <div className={s.firstname_and_lastname}>
+        <label>
+          <p>Name</p>
+          <input type="text"  name='firstname' placeholder='Ivan'/>
+        </label>
+
+        <label>
+          <p>Lastname</p>
+          <input type="text"  name='lastname' placeholder='Ivanov'/>
+        </label>
+      </div>
+
+      <label>
+        <p>Massage</p>
+        <textarea type="text"  name='massage' placeholder='Your massage'/>
+      </label>
+
+      <button>Send Massage</button>
+
+
+    </form>
   )
 }
