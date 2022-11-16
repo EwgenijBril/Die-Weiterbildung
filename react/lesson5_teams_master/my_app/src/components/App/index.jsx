456709e27215f789cb16a70deceb6ea69d4c476f
Evgeny <bril424@mail.ru>
2022-11-16 12:47:34 +0100

    router and AddTeamForm added

--- react/lesson5_teams_master/my_app/src/components/App/index.jsx
+++ react/lesson5_teams_master/my_app/src/components/App/index.jsx
@@ -1,8 +1,17 @@
+import ConfigurationsPage from "../../pages/ConfigurationPage";
+import TeamsPage from "../../pages/TeamsPage";
+import Nav from "../Nav";
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div>
-      
+      <Nav/>
+      <Routes>
+        <Route path="/configurations_page" element={<ConfigurationsPage/>}/>
+        <Route path="/teams_page" element={<TeamsPage/>}/>
+      </Routes>
     </div>
   );
 }
