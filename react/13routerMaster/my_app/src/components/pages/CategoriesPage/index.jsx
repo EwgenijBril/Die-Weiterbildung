ee37b413fb4f521a99795da7fae914ac4c6d5aef
Evgeny <bril424@mail.ru>
2022-12-22 15:01:18 +0100

    fetch and CategoryCard added

--- react/13routerMaster/my_app/src/components/pages/CategoriesPage/index.jsx
+++ react/13routerMaster/my_app/src/components/pages/CategoriesPage/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { getCategories } from '../../../requests/categories';
+import CategoryCard from '../../CategoryCard';
 
 export default function CategoriesPage() {
+
+  const [ categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories(setCategories)
+  }, [])
+
   return (
-    <div>CategoriesPage</div>
+    <div>
+      {
+       categories.map((el, id) => <CategoryCard key={id} category={el}/>)
+      }
+    </div>
   )
 }
