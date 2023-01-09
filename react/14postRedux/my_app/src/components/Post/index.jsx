285ccffc66f6f42a98cb8bf8adb21652783e06d7
Evgeny <bril424@mail.ru>
2023-01-09 18:05:34 +0100

    changeLike function added

--- react/14postRedux/my_app/src/components/Post/index.jsx
+++ react/14postRedux/my_app/src/components/Post/index.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import s from './index.module.css'
 import { CloseCircleOutlined } from '@ant-design/icons'
 import CommentsContainer from '../CommentsContainer'
+import { useDispatch } from 'react-redux'
+import { changeLike } from '../../store/reducers/postReducer'
 
 export default function Post({id, title, text, like, comments, delete_post}) {
+
+  const dispatch = useDispatch();
+
+  const change_like = id => dispatch(changeLike({id: id}))
+
+  const like_text = like ? 'Liked' : 'Like'
+
+  const like_style = [s.like_btn, like ? s.like_btn_active : ''].join(' ');
+
+  
 
   return (
     <div className={s.post_item} >
         <CloseCircleOutlined className={s.cross_icon} onClick={() => delete_post(id)}/>
         <p>Title: {title} </p>
         <p>Text: {text} </p>
+        <p className={like_style} onClick={() => change_like(id)}> {like_text} </p>
         <CommentsContainer comments={ comments } post_id={id}/>
     </div>
   )
--- react/14postRedux/my_app/src/store/reducers/postReducer.js
+++ react/14postRedux/my_app/src/store/reducers/postReducer.js
@@ -4,11 +4,13 @@ const ADD_POST = 'ADD_POST'
 const DELETE_POST = 'DELETE_POST'
 const ADD_COMMENT = 'ADD_COMMENT'
 const DELETE_COMMENT = 'DELETE_COMMENT'
+const CHANGE_LIKE = 'CHANGE_LIKE'
 
 export const addPost = payload => ({ type: ADD_POST, payload });
 export const deletePost = payload => ({ type: DELETE_POST, payload });
 export const addComment = payload => ({ type: ADD_COMMENT, payload });
-export const deleteComment = payload => ({ type: DELETE_COMMENT, payload})
+export const deleteComment = payload => ({ type: DELETE_COMMENT, payload});
+export const changeLike = payload => ({ type: CHANGE_LIKE, payload });
 
 
 export const postReducer = ( state = posts_data, action) => {
@@ -31,18 +33,23 @@ export const postReducer = ( state = posts_data, action) => {
         const target_state = state.find(el => el.id === action.payload.post_id);
         target_state.comments = target_state.comments.filter(el => el.id !== action.payload.id);
         return [...state]
+    } else if ( action.type === CHANGE_LIKE ) {
+        const target_card = state.find(el => el.id === action.payload.id);
+        target_card.like = !target_card.like
+        return [...state]   
     } else {
         return state
     }
 
-    // const delete_comment = (post_id, comment_id) => {
-    //     const target_post = posts.find(el => el.id === post_id);
-    //     target_post.comments = target_post.comments.filter(el => el.id !== comment_id);
-    //     setPost([...posts])
-    //   }
+    
 
    
-  
+    // const change_like = (id) => {
+    //     const target_card = posts.find(el => el.id === id);
+    //     target_card.like = !target_card.like
+    
+    //     setPost([...posts])
+    //   }
     
     
 }
