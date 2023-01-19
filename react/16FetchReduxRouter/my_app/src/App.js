df9a87dd01743b0b91fc8ce083fa77c795c29f00
Evgeny <bril424@mail.ru>
2023-01-19 19:57:17 +0100

    ProductsPage added

--- react/16FetchReduxRouter/my_app/src/App.js
+++ react/16FetchReduxRouter/my_app/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout';
 import CategoriesPage from './pages/CategoriesPage';
 import CartPage from './pages/CartPage';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Layout/>}>
           <Route index element={<CategoriesPage/>}/>
           <Route path='cart' element={<CartPage/>}/>
+          <Route path=':category' element={<ProductsPage/>}/>
         </Route>
       </Routes>
 
