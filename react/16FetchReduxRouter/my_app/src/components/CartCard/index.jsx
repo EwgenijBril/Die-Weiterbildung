cecf7b29db4e4346969025bb08ebfb84694ebf93
Evgeny <bril424@mail.ru>
2023-01-27 13:05:58 +0100

    increment_count decrement_count added

--- react/16FetchReduxRouter/my_app/src/components/CartCard/index.jsx
+++ react/16FetchReduxRouter/my_app/src/components/CartCard/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import s from './index.module.css'
+import { useDispatch } from 'react-redux'
+import { incrementCount, decrementCount } from '../../store/reducers/cart'
 
 
 export default function CartCard({id, image, title, price, count}) {
+
+  const dispatch = useDispatch();
+
+  const increment_count = () => dispatch(incrementCount(id))
+  const decrement_count = () => dispatch(decrementCount(id))
 
   return (
     <div className={s.card}>
@@ -10,8 +17,8 @@ export default function CartCard({id, image, title, price, count}) {
         <p>Title:{ title }</p>
         <p>{ price } x { count } = { price * count }</p>
         <div className={s.triggers}>
-          <button>+</button>
-          <button>-</button> 
+          <button onClick={increment_count}>+</button>
+          <button onClick={decrement_count}>-</button> 
         </div>
         
     </div>
--- react/16FetchReduxRouter/my_app/src/store/reducers/cart.js
+++ react/16FetchReduxRouter/my_app/src/store/reducers/cart.js
@@ -2,9 +2,13 @@ const defaultState = [];
 
 const ADD_TO_CART = 'ADD_TO_CART';
 const CLEAR_CART = 'CLEAR_CART'
+const INCREMENT_COUNT ='INREMENT_COUNT'
+const DECREMENT_COUNT ='DECREMENT_COUNT'
 
 export const addToCart = payload => ({type: ADD_TO_CART, payload});
-export const clearCart = () => ({type: CLEAR_CART})
+export const clearCart = () => ({type: CLEAR_CART});
+export const incrementCount = payload => ({ type: INCREMENT_COUNT, payload });
+export const decrementCount = payload => ({ type: DECREMENT_COUNT, payload });
 
 const checkProduct = ( state, payload ) => {
     const productInState = state.find(el => el.id === payload.id);
@@ -25,6 +29,17 @@ export const cartReducer = (state = defaultState, action) => {
         return checkProduct(state, action.payload)
     } else if ( action.type === CLEAR_CART ) {
         return defaultState 
+    } else if ( action.type === INCREMENT_COUNT ) {
+        state.find(el => el.id === action.payload).count++
+        return [...state]
+    } else if ( action.type === DECREMENT_COUNT ) {
+        const target_card = state.find(el => el.id === action.payload)
+
+        target_card.count === 1 
+        ? state = state.filter(el => el.id !== action.payload)  
+        : target_card.count--
+
+        return [...state] 
     } else {
         return state
     }
