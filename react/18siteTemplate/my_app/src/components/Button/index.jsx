7459aeef0cf0a754789c2643860c86b9016a5a6e
Evgeny <bril424@mail.ru>
2023-02-01 12:30:28 +0100

    my_app created

---
+++ react/18siteTemplate/my_app/src/components/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './index.module.css'
+
+export default function Button({ children }) {
+  return (
+    <div className={s.sigp_up_btn}>{children}</div>
+  )
+}
---
+++ react/18siteTemplate/my_app/src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Nav from '../Nav'
+import Button from '../Button'
+import logo from './media/logo_pic.png'
+import s from './index.module.css'
+import { BsGlobe2 } from 'react-icons/bs'
+
+
+export default function Header() {
+  return (
+    <header className={s.main_header}>
+        <div >
+        <Nav/>
+        <img src={logo} alt="logo" />
+        <p>Sales:<br/>+1 (800) 315-5939</p>
+        <div className={s.info_block}>
+            <BsGlobe2 className={s.glob_icon}/>
+            <p>{sales}</p>
+            <p>Search:</p>
+            <p>Log in:</p>
+        </div>
+        <Button>Sign up</Button>
+        </div>
+    </header>
+  )
+}
