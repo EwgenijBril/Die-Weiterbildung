6ca59f3969b6cabdbf83a7eeae474822511a9040
Evgeny <bril424@mail.ru>
2023-02-08 10:26:19 +0100

    ProductCard and Products added

--- react/18siteTemplate/my_app/src/App.js
+++ react/18siteTemplate/my_app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner1 from './components/Banner1';
 import Header from './components/Header';
+import Products from './components/Products';
 
 function App() {
   return (
     <div>
       <Header/>
       <Banner1/>
+      <Products/>
     </div>
   );
 }
