229c87804739965e18c82d4b58a110bb67df1b2c
Evgeny <bril424@mail.ru>
2023-03-01 18:36:30 +0100

    lesson2 updated

---
+++ Node.js/lesson2/routes/mobiles.js
@@ -0,0 +1,31 @@
+import { Router } from "express" // ипорт функции Router из express
+import { mobilesList } from "../data/mobilesList.js";
+// const { Router } = require('express');
+
+const router = Router(); // создаем экземпляр роута
+
+router.get('/', (req, res) => {
+    const { quantity } = req.query
+    const parsedQuantity = parseInt(quantity)
+    if (!isNaN(parsedQuantity)) {
+        const filteredMobilesList = mobilesList.filter(el => el.quantity >= parsedQuantity)
+        res.send(filteredMobilesList)
+    } else {
+        res.send(mobilesList)
+    }
+    
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const mobileItem = mobilesList.find(el => el.id === +id)
+    res.send(mobileItem)
+});
+
+router.post('/', (req, res) => {
+    mobilesList.push(req.body);
+    res.send(201)
+});
+
+export  default router
+// module.exprots = router;
---
+++ Node.js/lesson2/routes/stores.js
@@ -0,0 +1,24 @@
+import { Router } from "express" // const { Router } = require('express');
+import { storesList } from "../data/storesList.js";
+
+const router = Router();
+
+router.get(
+    '/', 
+    (req, res, next) => {
+        console.log('before getting storeList');
+        next()
+    },
+    (req, res) => {
+        console.log('getting storeList')
+    res.send(storesList)
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const storeItem = storesList.find(el => el.id === +id)
+    res.send(storeItem)
+});
+
+export  default router
+// module.exports = router;
